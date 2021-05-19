3f5272bb9040c20d15911e2f8045b45c717c1ae7
DominikHubert <38897039+DominikHubert@users.noreply.github.com>
2021-05-19 13:41:44 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""This script prompts a user to enter a message to encode or decode
+using a classic Caesar shift substitution (3 letter shift)"""
+SHIFT = 3
+WORD = input("Please enter text")
