4ea4aef66f80b4e4bc3a938c590af589df0b2d66
DominikHubert <38897039+DominikHubert@users.noreply.github.com>
2021-05-21 10:12:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 # Step 1) Open Firefox 
 browser = webdriver.Firefox(executable_path="./geckodriver")
 # Step 2) Navigate to Facebook
 browser.get("http://localhost:8081/")
-# Step 4) Click Login
+# Step 3) 
 wait = WebDriverWait( browser, 5 )
 proof_name = browser.find_element_by_tag_name('h1')
 assert proof_name.text == "Testpage"
